3b3e33d1acae5fb926db015c7bdb6e6a9e378dd4
welshronaldo <welshronaldo@gmail.com>
2022-04-04 15:58:47 +0100

    Add project files.

---
+++ ViewCompTest/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ViewCompTest.ViewComponents;
+
+namespace ViewCompTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public ActionResult OnGetViewComponent(int count)
+        {
+            return ViewComponent(nameof(TestComponent), new { count });
+        }
+    }
+}
---
+++ ViewCompTest/ViewComponents/TestComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ViewCompTest.ViewComponents;
+
+public class TestComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(int count)
+    {
+        var vm = new List<SelectListItem>();
+        for (var i = 0; i < count; i++)
+        {
+            var x = Guid.NewGuid().ToString();
+            vm.Add(new SelectListItem {Value = x, Text = x});
+        }
+
+        return View(vm);
+    }
+}
